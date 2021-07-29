10ffd5489ceebeb8a20f73ed9e93d04bc720a69b
Esteban Geraldo <88014735+esugeraldo@users.noreply.github.com>
2021-07-28 22:07:55 -0400

    Commit de fetch

--- src/main/java/com/clase/SegundaClaseGit/App.java
+++ src/main/java/com/clase/SegundaClaseGit/App.java
@@ -4,6 +4,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("coco");
-    System.out.println("nuevo commit por github pagina")
+    System.out.println("nuevo commit por github pagina");
+    System.out.println("no rindiendonos con fetch");
   }
 }
