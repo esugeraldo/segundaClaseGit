84bdac1259f15ca57a3f3626cca61a477839c176
Esteban Geraldo <88014735+esugeraldo@users.noreply.github.com>
2021-07-28 22:01:36 -0400

    Update App.java

--- src/main/java/com/clase/SegundaClaseGit/App.java
+++ src/main/java/com/clase/SegundaClaseGit/App.java
@@ -4,5 +4,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("coco");
+    System.out.println("nuevo commit por github pagina")
   }
 }
