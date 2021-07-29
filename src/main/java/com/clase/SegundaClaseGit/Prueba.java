e496c2fd6faf8a0d64d4341fe39f67ec613609ce
Esteban Geraldo <esugeraldo@gmail.com>
2021-07-28 20:24:48 -0400

    clase prueba

---
+++ src/main/java/com/clase/SegundaClaseGit/Prueba.java
@@ -0,0 +1,26 @@
+package com.clase.SegundaClaseGit;
+
+public class Prueba {
+
+	private String estoNoEsNada;
+
+	
+	public Prueba(String estoNoEsNada) {
+		super();
+		this.estoNoEsNada = estoNoEsNada;
+	}
+
+	public String getEstoNoEsNada() {
+		return estoNoEsNada;
+	}
+
+	public void setEstoNoEsNada(String estoNoEsNada) {
+		this.estoNoEsNada = estoNoEsNada;
+	}
+
+	@Override
+	public String toString() {
+		return "Prueba [estoNoEsNada=" + estoNoEsNada + "]";
+	}
+	
+}
